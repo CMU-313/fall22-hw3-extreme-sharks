6fe7d33fdd5866109301138b546ae9b90b53fdd1
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-10-03 00:30:49 -0400

    Use double in the database for consistency

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,4 +1,4 @@
-create memory table T_REVIEW ( REV_ID_C varchar(36) not null, REV_IDROUTESTEP_C varchar(36) not null, REV_CATEGORY_C varchar(36) not null, REV_VALUE_DEC decimal(3, 2) not null, primary key (REV_ID_C) );
+create memory table T_REVIEW ( REV_ID_C varchar(36) not null, REV_IDROUTESTEP_C varchar(36) not null, REV_CATEGORY_C varchar(36) not null, REV_VALUE_DBL double not null, primary key (REV_ID_C) );
 
 alter table T_REVIEW add constraint FK_REV_IDROUTESTEP_C foreign key (REV_IDROUTESTEP_C) references T_ROUTE_STEP (RTP_ID_C) on delete cascade on update cascade;
 

8ae5eeefcc6e2f25c1b388ab2abcec4697fabc67
kitty3190 <92182939+kitty3190@users.noreply.github.com>
2022-10-06 17:49:24 -0400

    Implemented the API endpoint for getting reviews (#30)
    * Implemented initial changes on the API for getting reviews.
    
    * Remove the DbResource.java test file
    
    * Added a method to get comments in ReviewDao.java.
    
    * Fix the reviews endpoint parameters
    
    * Fix a SQL query syntax error
    
    * Fixed the JSON outputs of the API endpoint.
    
    * Fix the id parameter
    
    * Fixed issues regarding getting comments and reviews.
    
    * Remove the DbResource test file
    
    * Fixed comment issues.
    
    Co-authored-by: Nicolas Ettlin <Nicolapps@users.noreply.github.com>
    
    * Fix ReviewComment documentation
    
    Co-authored-by: Ziqi Ding <ziqiding@ZiqitekiMacBook-Air.local>
    Co-authored-by: Nicolas Ettlin <nicolas.ettlin@me.com>
    Co-authored-by: Ziqi Ding <ziqiding@ziqitekiair.wifi.local.cmu.edu>
    Co-authored-by: Nicolas Ettlin <Nicolapps@users.noreply.github.com>

--- docs-web/src/main/java/com/sismics/docs/rest/resource/ReviewsResource.java
+++ docs-web/src/main/java/com/sismics/docs/rest/resource/ReviewsResource.java
@@ -1,6 +1,20 @@
 package com.sismics.docs.rest.resource;
 
+import java.util.*;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.sismics.docs.core.dao.ReviewDao;
+import com.sismics.docs.core.dao.ReviewDao.ReviewComment;
+import com.sismics.docs.core.model.jpa.Review;
+import com.sismics.docs.core.model.jpa.Route;
 
 /**
  * Reviews REST resources.
@@ -8,7 +22,7 @@ import javax.ws.rs.Path;
  * @author Ziqi Ding
  */
 @Path("/reviews")
-public class ReviewsResource {
+public class ReviewsResource extends BaseResource {
     /**
      * Returns the reviews for a document.
      *
@@ -17,19 +31,70 @@ public class ReviewsResource {
      * @apiGroup Review
      * @apiParam {String} id Document ID
      * @apiSuccess {String} id ID
-     * @apiSuccess {String} title Title
-     * @apiSuccess {String} description Description
      * @apiSuccess {Object[]} workflows The workflow array that shows all the workflows and reviews
      * @apiSuccess {String} workflows.name The name of one of the workflows.
      * @apiSuccess {Object[]} workflows.ratings The ratings array that contains all the ratings for a workflow on the document
      * @apiSuccess {String} workflows.ratings.category The category that a specific rating belongs to
      * @apiSuccess {Number} workflows.ratings.value The value of a specific rating (Range: 1-5)
-     * @apiSuccess {String[]} workflows.comments The comments array that contains all the comments for a workflow on the document
-     * @apiSuccess {Object[]} ratings Ratings
+     * @apiSuccess {Object[]} workflows.comments The comments array that contains all the comments for a workflow on the document
+     * @apiSuccess {String} workflows.comments.author The name of the author
+     * @apiSuccess {String} workflows.comments.contents The contents of the comment
      * @apiPermission user
      * @apiVersion 1.5.0
      *
      * @param documentId Document ID
      * @return Response
      */
+    @GET
+    @Path("{id: [a-z0-9\\-]+}")
+    public Response get(@PathParam("id") String documentId){
+        authenticate();
+        
+        // Review info
+        ReviewDao reviewDao = new ReviewDao();
+        Map<Route, List<Review>> reviews = reviewDao.findByDocument(documentId);
+        if (reviews == null) {
+            throw new NotFoundException();
+        }
+
+        // Add basic document info
+        JsonObjectBuilder reviewsJson = Json.createObjectBuilder()
+                .add("id", documentId);
+
+        
+        JsonArrayBuilder workflowsJson = Json.createArrayBuilder();
+
+        // Iterate through the reviews aray to add workflow
+        for (Map.Entry<Route, List<Review>> entry : reviews.entrySet()){
+            Route route = entry.getKey();
+
+            // get ratings
+            JsonArrayBuilder ratingsJson = Json.createArrayBuilder();
+            for (Review r : entry.getValue()){
+                ratingsJson.add(Json.createObjectBuilder()
+                                    .add("category", r.getCategory())
+                                    .add("value", r.getValue()));
+            }
+
+            // get comments
+            JsonArrayBuilder commentsJson = Json.createArrayBuilder();
+            List<ReviewComment> comments = reviewDao.getComments(route.getId());
+            for (ReviewComment c : comments){
+                commentsJson.add(Json.createObjectBuilder()
+                                    .add("author", c.author)
+                                    .add("contents", c.contents));
+            }
+            
+            // workflow jsons
+            workflowsJson.add(Json.createObjectBuilder()
+                            .add("name", route.getName())
+                            .add("ratings", ratingsJson)
+                            .add("comments", commentsJson));
+        }
+
+        reviewsJson.add("workflows", workflowsJson);
+
+        return Response.ok().entity(reviewsJson.build()).build();
+    }
+    
 }
